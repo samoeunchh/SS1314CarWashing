e8199ac48f6654da990a56b34bac91b29a7560bd
Samoeunchh <samoeunchh@gmail.com>
2023-03-03 15:45:25 +0700

    Sale Report

--- SS1314CarWashing/Controllers/SaleReportController.cs
+++ SS1314CarWashing/Controllers/SaleReportController.cs
@@ -1,16 +1,51 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SS1314CarWashing.Models;
 
 namespace SS1314CarWashing.Controllers
 {
     public class SaleReportController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public SaleReportController(AppDbContext context)
+        {
+            _context = context;
+        }
         // GET: SaleReportController
         public ActionResult Index()
         {
             return View();
         }
-
+        public async Task<JsonResult> GetData(string fromDate,string toDate)
+        {
+            if (string.IsNullOrEmpty(fromDate) && string.IsNullOrEmpty(toDate))
+            {
+                return null;
+            }
+            else
+            {
+                var fDate = DateTime.Parse(fromDate + " 00:00:00");
+                var tDate = DateTime.Parse(toDate + " 23:59:59");
+                var sale = await (from s in _context.Sale
+                                  join c in _context.Customer
+                                  on s.CustomerId equals c.CustomerId
+                                  where s.IssueDate >= fDate && s.IssueDate <= tDate
+                                  orderby s.IssueDate descending
+                                  select new SaleDTO
+                                  {
+                                      SaleId = s.SaleId,
+                                      CustomerName = c.CustomerName,
+                                      Total = s.Total,
+                                      GrandTotal = s.GrandTotal,
+                                      Discount = s.Discount,
+                                      IssueDate = s.IssueDate,
+                                      InvoiceNumber = s.InvoiceNumber,
+                                  }).ToListAsync();
+                return Json(sale);
+            }
+        }
         // GET: SaleReportController/Details/5
         public ActionResult Details(int id)
         {
